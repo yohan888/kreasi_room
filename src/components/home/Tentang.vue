862db801242b58432d92f20b925bf325cefd97fb
MarMarWijaya <mariowijaya31@gmail.com>
2021-10-25 14:41:27 +0700

    add library icon

--- src/components/home/Tentang.vue
+++ src/components/home/Tentang.vue
@@ -1,5 +1,36 @@
 <template>
-    <div>
-        <h1>Ini Tentang</h1>
+    <div class="row d-flex justify-content-center">
+        <div class="col-md">
+            <img class="imageTentang" src="../../assets/images/img-tentang.jpg">
+        </div>
+        <div class="col-md" style="text-align: left">
+            <h1>Apa itu (Nama Website)?</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, culpa. Atque est dolores enim iste</p>
+            <span>
+                <i class="fas fa-camera"></i>
+
+            </span>
+        </div>
     </div>
 </template>
+
+<style scoped>
+    .imageTentang{
+        object-fit: cover;
+        width: 400px;
+        height: 400px;
+        border-radius: 15px;
+    }
+
+    .iconTentang{
+        background-color: red;
+    }
+
+    @media screen and (max-width: 768px) {
+        .imageTentang{
+            width: 150px;
+            height: 150px;
+            object-fit: cover;
+        }
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
-
+import '@fortawesome/fontawesome-free/css/fontawesome.css'
+import '@fortawesome/fontawesome-free/css/brands.css'
+import '@fortawesome/fontawesome-free/css/solid.css'
 Vue.config.productionTip = false
 
 
