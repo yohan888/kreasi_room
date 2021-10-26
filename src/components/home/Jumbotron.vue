a36d06f7a2aa439bbabdf8e2c02243a6a4069813
Daniel Harliano Sitorus <harlianositorusdaniel@gmail.com>
2021-10-26 11:32:06 +0700

    add NavbarGuest.vue & Jumbotron.vue

--- src/components/home/Jumbotron.vue
+++ src/components/home/Jumbotron.vue
@@ -1,3 +1,86 @@
 <template>
-    <h1>Ini Jumbotron</h1>
+    <div class="jumbotron">
+        <center>
+        <div class="container" style=" text-align: left">
+            <br><br>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="judul">
+                        <h1>
+                            <b style="color:white;">Bangun karir kamu dengan ikut berbagai macam webinar</b>
+                        </h1>
+                    </div>
+                </div>
+                <div class="col-md-auto">
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-md-6">
+                    <p class="lead isi">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. At amet, lectus eget sodales ac. Pellentesque nam faucibus elementum sed elementum, nunc nec.
+                    </p>
+                </div>
+                <div class="col-md-auto">
+                </div>
+            </div>
+
+            
+
+            <div id="carouselExampleControls" class="carousel slide " data-bs-ride="carousel">
+                <div class="carousel-inner carousel1">
+                    <div class="carousel-item active ">
+                        <img src="../../assets/images/img-tentang.jpg" class="d-block w-100 " alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../../assets/images/img-tentang.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../../assets/images/img-tentang.jpg" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </center>
+    </div>
 </template>
+<style scoped>
+.judul {
+    text-align: left;
+}
+.isi {
+    text-align: left;
+    color: lightgray;
+}
+.jumbotron{
+    background-image: url('../../assets/images/Untitled-1.png');
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 120vh;
+    /* width: 100%; */
+}
+.carousel1 {
+    border-radius: 20px;
+    width: 100%;
+    margin-top: 5%;
+    height: 550px;
+}
+
+@media screen and (max-width: 768px) {
+    .jumbotron{
+        height: 100vh;
+    }
+    .carousel1 {
+        height: 250px;
+    }
+}
+
+</style>
--- src/components/home/NavbarGuest.vue
+++ src/components/home/NavbarGuest.vue
@@ -1,3 +1,32 @@
 <template>
-    <h1>Ini navbar</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#4074ac">
+  <div class="container">
+    <a href="#" class="me-2"><img src="../../assets/images/logo-footer.png"></a>
+    <a class="navbar-brand" href="#" style="font-size:24pt;">Kreasi Room</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mx-auto">
+          <li class="nav-item me-5">
+            <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+          </li>
+          <li class="nav-item me-5">
+            <a class="nav-link" href="#">Tentang</a>
+          </li>
+          <li class="nav-item me-5">
+            <a class="nav-link" href="#">Kontak</a>
+          </li>
+      </ul>
+      <button class="btn btn-outline-light me-2">Masuk</button>
+      <button class="btn btn-outline-light daftar">Daftar Sekarang</button>
+    </div>
+  </div>
+</nav>
 </template>
+<style scoped>
+.daftar {
+  background-color:white;
+  color:black;
+}
+</style>
