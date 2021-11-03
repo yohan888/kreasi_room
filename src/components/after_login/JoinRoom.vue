bc5b476a37126a06c260c62f847c71965930d972
Daniel Harliano Sitorus <harlianositorusdaniel@gmail.com>
2021-11-03 10:44:24 +0700

    Add JoinEvent

--- src/components/after_login/JoinRoom.vue
+++ src/components/after_login/JoinRoom.vue
@@ -1,3 +1,204 @@
 <template>
-    <h1>INI JOIN ROOM</h1>
+<div class="container mt-5">
+  <div class="row">
+    <div class="col">
+      <video id='my-video' class='video-js custom-video' controls preload='auto' width='100%' height='90%' poster='MY_VIDEO_POSTER.jpg' data-setup='{}'>
+        <source src='https://drive.google.com/uc?export=download&id=1w_FITkEoRrGmaA9sgTCXQ6joOZIUcWuq' type='video/mp4'>
+        <source src='https://drive.google.com/uc?export=download&id=1w_FITkEoRrGmaA9sgTCXQ6joOZIUcWuq' type='video/webm'>
+        <p class='vjs-no-js'>To view this video please enable JavaScript, and consider upgrading to a web browser that
+          <a href='https://videojs.com/html5-video-support/' target='_blank'>supports HTML5 video</a>
+        </p>
+      </video> 
+    </div>
+  </div>
+
+ 
+  <div class="row">
+    <div class="col">
+      <div class="row">
+            <div class="col-sm-auto">
+              <img class="imagePenyelenggara rounded-circle" src="../../assets/images/img-tentang.jpg">
+            </div>
+            <div class="col" style="text-align: left;">
+                <span style="color:#0A3D62;"><b> Michella</b></span> <br>
+                <span style="color:#B2B5B8;"> Michella@gmail.com</span>
+            </div>
+            <div class="col">
+                <div class="custom-btn">
+                    <button class="btn btn-lg me-2 btn-like">Like</button>
+                    <button class="btn btn-lg btn-bagikan">Bagikan</button>
+                </div>
+            </div>
+
+        <div class="row">
+            <div class="col-auto custom-judulVideo">
+                <h5 style="color:#0A3D62;"><b>Judul Video</b></h5>
+                    <p style="color:#B2B5B8;">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Labore dolorem maiores totam repudiandae, 
+                      enim optio illo laboriosam ullam fugit praesentium rem nobis voluptatum rerum ea natus eos laborum esse fuga.    
+                    </p>
+                    <p style="color:#B2B5B8;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. At amet, 
+                      lectus eget sodales ac. 
+                    </p>
+            </div>
+        </div>  
+      </div>
+    </div>
+    <div class="col-auto">
+      <div class="card" style="width: auto;">
+        <div class="card-body">
+            <h5 class="card-title custom-title-card"><b>Pertanyaan</b></h5>
+            <hr size="4">
+              <div class="row">
+                  <div class="col-sm-auto">
+                    <img class="imagePenyelenggara rounded-circle" src="../../assets/images/img-tentang.jpg">
+                  </div>
+                  <div class="col" style="text-align: left;">
+                    <span style="color:#0A3D62;"><b> Michella</b></span> <br>
+                    <span style="color:#B2B5B8;">Lorem ipsum dolor sit amet, consectetur adipiscing eli ? </span>
+                  </div>
+              </div>
+              <div class="row" style="margin-top:10%;">
+                  <div class="col-sm-auto">
+                    <img class="imagePenyelenggara rounded-circle" src="../../assets/images/img-tentang.jpg">
+                  </div>
+                  <div class="col" style="text-align: left;">
+                    <span style="color:#0A3D62;"><b> Michella</b></span> <br>
+                    <span style="color:#B2B5B8;">Lorem ipsum dolor sit amet, consectetur adipiscing eli ? </span>
+                  </div>
+              </div>
+              <div class="row" style="margin-top:10%;">
+                  <div class="col-sm-auto">
+                      <img class="imagePenyelenggara rounded-circle" src="../../assets/images/img-tentang.jpg">
+                  </div>
+                  <div class="col" style="text-align: left;">
+                      <span style="color:#0A3D62;"><b> Michella</b></span> <br>
+                      <span style="color:#B2B5B8;">Lorem ipsum dolor sit amet, consectetur adipiscing eli ? </span>
+                  </div>
+              </div>
+              <div class="col" style="margin-top:30%"><hr>
+                <div class="form">
+                  <input type="text" name="normal_input" id="normal_input" class="form-control" placeholder="Ketik Pertanyaan">
+                </div>
+              </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+<!-- <div class="container">
+  <div class="col">
+      <div class="row">
+            <div class="col-sm-auto">
+              <img class="imagePenyelenggara rounded-circle" src="../../assets/images/img-tentang.jpg">
+            </div>
+            <div class="col" style="text-align: left;">
+                <span style="color:#0A3D62;"><b> Michella</b></span> <br>
+                <span style="color:#B2B5B8;"> Michella@gmail.com</span>
+            </div>
+            <div class="col-md-9">
+                <div class="custom-btn">
+                    <button class="btn btn-lg me-2 btn-like">Like</button>
+                    <button class="btn btn-lg btn-bagikan">Bagikan</button>
+                </div>
+            </div>
+
+        <div class="row">
+            <div class="col-md-8 custom-judulVideo">
+                <h5>Judul Video</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Labore dolorem maiores totam repudiandae, 
+                      enim optio illo laboriosam ullam fugit praesentium rem nobis voluptatum rerum ea natus eos laborum esse fuga.    
+                    </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. At amet, 
+                      lectus eget sodales ac. 
+                    </p>
+            </div>
+        </div>  
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+      <div class="col">
+        
+        <div class="card" style="width: 30%;">
+          <div class="card-body">
+              <h5 class="card-title custom-title-card"><b>Pertanyaan</b></h5>
+              <hr>
+              <div class="row">
+                  <div class="col-sm-auto">
+                    <img class="imagePenyelenggara rounded-circle" src="../../assets/images/img-tentang.jpg">
+                  </div>
+                  <div class="col" style="text-align: left;">
+                    <span style="color:#0A3D62;"><b> Michella</b></span> <br>
+                    <span style="color:#B2B5B8;">Lorem ipsum dolor sit amet, consectetur adipiscing eli ? </span>
+                  </div>
+              </div>
+              <div class="row" style="margin-top:10%;">
+                  <div class="col-sm-auto">
+                    <img class="imagePenyelenggara rounded-circle" src="../../assets/images/img-tentang.jpg">
+                  </div>
+                  <div class="col" style="text-align: left;">
+                    <span style="color:#0A3D62;"><b> Michella</b></span> <br>
+                    <span style="color:#B2B5B8;">Lorem ipsum dolor sit amet, consectetur adipiscing eli ? </span>
+                  </div>
+              </div>
+              <div class="row" style="margin-top:10%;">
+                  <div class="col-sm-auto">
+                      <img class="imagePenyelenggara rounded-circle" src="../../assets/images/img-tentang.jpg">
+                  </div>
+                  <div class="col" style="text-align: left;">
+                      <span style="color:#0A3D62;"><b> Michella</b></span> <br>
+                      <span style="color:#B2B5B8;">Lorem ipsum dolor sit amet, consectetur adipiscing eli ? </span>
+                  </div>
+              </div>
+              <div class="col" style="margin-top:30%"><hr>
+                <div class="form">
+                  <input type="text" name="normal_input" id="normal_input" class="form-control" placeholder="Ketik Pertanyaan">
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+  </div>-->
+
+
+
 </template>
+
+<style scoped>
+
+.custom-video {
+  border-radius: 10px;
+  background-color: #0a3d62;
+}
+.imagePenyelenggara {
+  width: 50px;
+  height: 50px;
+}
+.btn-like {
+  color: #0a3d62;
+  border-radius: 10px;
+  border: 3px solid #0a3d62;
+}
+.btn-bagikan {
+  border: 3px solid #0a3d62;
+  border-radius: 10px;
+  margin: 10px;
+  color: #0a3d62;
+}
+.custom-judulVideo {
+  text-align: left;
+  margin-bottom: auto;
+}
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+.custom-title-card {
+  text-align: left;
+}
+</style>
+  
+  
